d04834550aecfbd750fab10ec4d016f6f75070dc
poseidon-code <61552390+poseidon-code@users.noreply.github.com>
2022-09-11 10:00:00 +0530

    add: helmet

--- index.js
+++ index.js
@@ -1,57 +1,61 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
 
-const app = express()
-const PORT = process.env.PORT || 5000
-const URI = `mongodb+srv://everyone:${process.env.EVERYONE}@stories.l6tlk.mongodb.net/stories?retryWrites=true&w=majority`
+const app = express();
+const PORT = process.env.PORT || 5000;
+const URI = `mongodb+srv://everyone:${process.env.EVERYONE}@stories.l6tlk.mongodb.net/stories?retryWrites=true&w=majority`;
 
-app.use(cors())
-app.use(express.json())
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.disable('x-powered-by');
 
 // Connect MongoDB Atlas' stories Database
 mongoose
     .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log('Database Connected...')
+        console.log('Database Connected...');
     })
-    .catch((err) => console.log(err))
+    .catch(err => console.log(err));
 
 // Stories Schema
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
+// prettier-ignore
 const StoriesSchema = new Schema({
-    title: String,
-    author: String,
-    story: String,
-    moral: String,
-})
+    title   : String,
+    author  : String,
+    story   : String,
+    moral   : String,
+});
 
-const Stories = mongoose.model('stories', StoriesSchema)
+const Stories = mongoose.model('stories', StoriesSchema);
 
 // Routes
 // get random story
-app.get('/', async (req, res) => {
+app.get('/', async (_, res) => {
     try {
-        const count = await Stories.countDocuments()
-        const random = Math.floor(Math.random() * count)
-        const story = await Stories.findOne().skip(random)
-        res.send(story)
+        const count = await Stories.countDocuments();
+        const random = Math.floor(Math.random() * count);
+        const story = await Stories.findOne().skip(random);
+        res.send(story);
     } catch (error) {
-        res.status(500).send(error)
+        res.status(500).send(error);
     }
-})
+});
 
 // get all stories
-app.get('/stories', async (req, res) => {
+app.get('/stories', async (_, res) => {
     try {
-        const stories = await Stories.find({})
-        res.send(stories)
+        const stories = await Stories.find({});
+        res.send(stories);
     } catch (error) {
-        res.status(500).send(error)
+        res.status(500).send(error);
     }
-})
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
